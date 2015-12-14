1b0b4e9ff46e9cf661edcd96d05f6600e1562f0e
Le Thanh Son <thsonvt@gmail.com>
2015-12-15 07:04:10 +0800

    switch to using a MongoDB collection for tasks

--- simple-todos-angular.js
+++ simple-todos-angular.js
@@ -1,12 +1,12 @@
+Tasks = new Mongo.Collection('tasks');
+
 if (Meteor.isClient) {
   angular.module('simple-todos', ['angular-meteor']);
 
-  angular.module('simple-todos').controller('TodosListCtrl', ['$scope', function($scope){    
-    $scope.tasks = [
-      {text: 'this is task 1'},
-      {text: 'this is task 2'},
-      {text: 'this is task 3'}
-    ];
+  angular.module('simple-todos').controller('TodosListCtrl', ['$scope', '$meteor', 
+
+    function($scope, $meteor){    
+      $scope.tasks = $meteor.collection(Tasks);
   }]);
 }
 
