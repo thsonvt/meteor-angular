119afa5b2d6fdb99553ffba2d4acb68f52588d7f
Le Thanh Son <thsonvt@gmail.com>
2015-12-15 07:25:17 +0800

    enable mobile version for the app

--- simple-todos-angular.js
+++ simple-todos-angular.js
@@ -3,6 +3,15 @@ Tasks = new Mongo.Collection('tasks');
 if (Meteor.isClient) {
   angular.module('simple-todos', ['angular-meteor']);
 
+  function onReady () {
+    angular.bootstrap(document, ['simple-todos']);
+  }
+
+  if (Meteor.isCordova)
+    angular.element(document).on('deviceready', onReady);
+  else
+    angular.element(document).ready(onReady);
+
   angular.module('simple-todos').controller('TodosListCtrl', ['$scope', '$meteor', 
 
     function($scope, $meteor){    
